f0699d75f632d1cf78bce0b2a93bf0e400f937da
Nicholas Felicelli <Nicholas.Felicelli@cchmc.org>
2016-01-19 15:25:21 -0500

    Fixing and scheduling console command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Cupa\Console\Commands\Dump::class,
+        \Cupa\Console\Commands\SendEmails::class,
     ];
 
     /**
@@ -23,5 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            Artisan::call('cupa:emails');
+        })->weekly()->mondays()->at('9:00');
     }
 }
