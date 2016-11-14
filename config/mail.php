d69443f556589990b6cb474f8fdadb98693ada2a
Nick Felicelli <nick.felicelli@gmail.com>
2016-11-14 09:38:27 -0500

    Fixing configs

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'webmaster@cincyultimate.org',
+        'name' => 'CUPA Web System',
     ],
 
     /*
