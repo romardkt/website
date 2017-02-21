64b47dec91d517c56beaacfafd6f1e8133aa861d
Nick Felicelli <kcin1018@users.noreply.github.com>
2017-02-21 14:26:48 -0500

    Updating script to run after deploy to setup the app (#109)

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# build the js/css
+yarn
+gulp
+
+# clear and optimize the Laravel app
+php artisan clear-compiled
+php artisan route:cache
+php artisan config:cache
+php artisan cache:clear
+php artisan view:clear
+php artisan optimize
+php artisan migrate
